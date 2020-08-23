6d94943ed5f843d2411f5411340d0ea82a145b91
relopes2020 <rebeca.lopes@gmail.com>
2020-08-23 18:40:26 -0300

    ajustes OS

--- src/br/com/mgoficina/model/OrdemDeServico.java
+++ src/br/com/mgoficina/model/OrdemDeServico.java
@@ -13,6 +13,8 @@ public class OrdemDeServico {
 	private Double valor;
 	private String metodoDePagamento;
 	private Cliente cliente;
+	private Veiculo veiculo;
+	
 		
 	public UUID getIdOrdemServico() {
 		return idOrdemServico;
@@ -50,9 +52,23 @@ public class OrdemDeServico {
 	public void setMetodoDePagamento(String metodoDePagamento) {
 		this.metodoDePagamento = metodoDePagamento;
 	}
-		
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+	
 	public OrdemDeServico(UUID idOrdemServico, String nomeServico, LocalDate inicioServico, LocalDate fimServico,
-			Double valor, String metodoDePagamento, Cliente cliente) {
+			Double valor, String metodoDePagamento, Cliente cliente, Veiculo veiculo) {
 		this.idOrdemServico = idOrdemServico;
 		this.nomeServico = nomeServico;
 		this.inicioServico = inicioServico;
@@ -60,10 +76,35 @@ public class OrdemDeServico {
 		this.valor = valor;
 		this.metodoDePagamento = metodoDePagamento;
 		this.cliente = cliente;
+		this.veiculo = veiculo;
+		
 	}
 	
-	
-	
-	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemDeServico other = (OrdemDeServico) obj;
+		if (idOrdemServico != other.idOrdemServico)
+			return false;
+		return true;
+	}
 
+	@Override
+	public String toString() {
+		StringBuilder string = new StringBuilder();
+		string.append("\n             ID: " + this.getIdOrdemServico() + "\n");
+		string.append("     Data de Início: " + this.getInicioServico() + "\n");
+		string.append("        Data de Término: " + this.getFimServico() + "\n");
+		string.append("          Valor: " + this.getValor() + "\n");
+		string.append("MetodoPagamento: " + this.getMetodoDePagamento() + "\n");
+		string.append("        Cliente: " + this.getCliente() + "\n");
+		string.append("        Veículo: " + this.getVeiculo() + "\n");
+		return string.toString();
+	}
+	
 }
